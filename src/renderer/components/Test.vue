beaf135d7ae0606f05dcf8d7d1889c706a7be346
Dmitry Gultyaev <dmitry.gultyaev@certara.com>
2020-11-11 16:53:45 +0100

    hmm

--- src/renderer/components/Test.vue
+++ src/renderer/components/Test.vue
@@ -1,27 +1,96 @@
 <template>
-  <div id="wrapper">
-    <div class="bg-red-600 text-white">
-      Test page
-    </div>
-    <btn color="success" route="/" cls="m-2" >Back to home <badge color="red-200 text-red-600">2</badge></btn>
+    <v-app id="inspire">
+    <v-app-bar
+      app
+      color="white"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <v-avatar
+          class="mr-10"
+          color="grey darken-1"
+          size="32"
+        ></v-avatar>
 
-    <router-view></router-view>
-  </div>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          text
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      List Item {{ n }}
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  link
+                  color="grey lighten-4"
+                >
+                  <v-list-item-content>
+                    <v-list-item-title>
+                      Refresh
+                    </v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-  import DButton from './UI/DButton.vue'
-  import DBadgeVue from './UI/DBadge.vue'
-
   export default {
-    name: 'test',
-    components: {
-      btn: DButton,
-      badge: DBadgeVue
-    }
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+    }),
   }
 </script>
-
-<style>
-
-</style>
